256b4dea1296b8019805521fb55022b470d37868
EmeraldSlash <riorex18@gmail.com>
2018-04-19 08:37:46 +1200

    Fixed a bunch of issues

--- Ready.lua
+++ Ready.lua
@@ -1,25 +1,28 @@
-local TIMEOUT = 1 -- Length of time where no descendants have been added to consider the object loaded
+local DEFAULT_TIMEOUT = 1 -- Length of time where no descendants have been added to consider the object loaded
 
 -- Author: EmeraldSlash
 -- Repository: https://github.com/EmeraldSlash/RbxReady
--- Last Updated: 04/16/2018
+-- Last Updated: 04/19/2018
 
 local Ready = {}
 
 function Ready:Wait(Object, Timeout)
-	if not Timeout then Timeout = TIMEOUT end		
+	if not Timeout then Timeout = DEFAULT_TIMEOUT end		
 	
 	local Timestamp = tick()
+	local LastDescendant
 
-	Object.DescendantAdded:Connect(function()
+	Object.DescendantAdded:Connect(function(Descendant)
 		Timestamp = tick()
+		LastDescendant = Descendant
 	end)	
 	
-	repeat wait() until (tick() - Timestamp) > TIMEOUT
+	repeat wait() until (tick() - Timestamp) > Timeout
+	return LastDescendant
 end
 
 function Ready:Connect(Object, Function, Timeout)
-	if not Timeout then Timeout = TIMEOUT end	
+	if not Timeout then Timeout = DEFAULT_TIMEOUT end	
 	
 	local Timestamp = tick()
 	local Connection
@@ -28,14 +31,14 @@ function Ready:Connect(Object, Function, Timeout)
 		local LocalTimestamp = tick()
 		Timestamp = LocalTimestamp
 		
-		wait(TIMEOUT)
+		wait(Timeout)
 		if Timestamp == LocalTimestamp and Connection.Connected then
 			Connection:Disconnect()
 			Function(Descendant)			
 		end
 	end
 
-	coroutine.wrap(LogDescendant)()
+	coroutine.resume(coroutine.create(LogDescendant))
 	Connection = Object.DescendantAdded:Connect(LogDescendant)
 	
 	return Connection
